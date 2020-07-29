1d85b2649d763d1f42c44039e129ca23a74b2210
somandra <somandra.bhl2015@gmail.com>
2020-07-29 14:40:45 +0530

    fetch data from api

--- src/App.js
+++ src/App.js
@@ -5,9 +5,15 @@ import {
     CountryPicker
 } from './components';
 
+import {fetchData} from './api';
+
 import styles from './App.module.css'
 
 class App extends Component {
+    async componentDidMount(){
+        const data = await fetchData();
+        console.log(data);
+    }
     render(){
         return(
             <div className={styles.container}>
